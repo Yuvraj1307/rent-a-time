279c63e2566b25da2240720e3cb59762307eb674
yuvraj maharshi <112754852+Yuvraj1307@users.noreply.github.com>
2023-07-24 00:29:29 +0530

    Update getslots.service.ts

--- frontend/myapp/src/app/services/getslots.service.ts
+++ frontend/myapp/src/app/services/getslots.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class GetslotsService {
-  private backendUrl1 =`http://localhost:4500/host/prop/slots`
+  private backendUrl1 =`https://rentatime.onrender.com/host/prop/slots`
 
   constructor(private http: HttpClient) { }
   Slots(id: any): Observable<any> {
